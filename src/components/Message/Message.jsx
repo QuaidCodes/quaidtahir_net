e8fc89afcc84eeeae13d1cd8ddd23fe91080c3aa
Tahir <tahirqua@amazon.com>
2024-03-27 03:35:13 -0400

    work on message component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 import "./Message.css";
 
 function Message() {
+  const [email, setEmail] = useState("");
+
+  function emailHandler(event) {
+    setEmail(event.target.value);
+    console.log(email);
+  }
+
+  function preset(event, target) {
+    event.preventDefault();
+
+    // g for gmail, o for outlook, y for yahoo
+    if (target === "g") setEmail(email + "@gmail.com");
+    else if (target === "o") setEmail(email + "@outlook.com");
+    else if (target === "y") setEmail(email + "@yahoo.com");
+  }
+
   return (
     <section className="message">
       <h1>
@@ -10,26 +28,37 @@ function Message() {
 
       <form>
         <div className="message-name">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="First Name" required />
+          <input type="text" placeholder="Last Name" required />
         </div>
         <div className="message-email">
-          <input type="email" placeholder="Email" />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => emailHandler(e)}
+            value={email}
+            required
+          />
           <div className="message-email-presets">
-            <button className="preset-btn">@gmail.com</button>
-            <button className="preset-btn">@outlook.com</button>
-            <button className="preset-btn">@yahoo.com</button>
+            <button className="preset-btn" onClick={(e) => preset(e, "g")}>
+              @gmail.com
+            </button>
+            <button className="preset-btn" onClick={(e) => preset(e, "o")}>
+              @outlook.com
+            </button>
+            <button className="preset-btn" onClick={(e) => preset(e, "y")}>
+              @yahoo.com
+            </button>
           </div>
         </div>
 
         <input type="tel" placeholder="Phone (Optional)" />
 
         <textarea
-          name=""
-          id=""
           cols="30"
           rows="10"
           placeholder="Message"
+          required
         ></textarea>
 
         <div className="message-btns">
