ac19971fcb7a7dab71762d7cd686551c16923a50
Tahir <quaidcodes@gmail.com>
2024-04-22 03:58:20 -0400

    commit email check

--- src/components/ProjectsDir/ProjectNavigation/ProjectNavigation.jsx
+++ src/components/ProjectsDir/ProjectNavigation/ProjectNavigation.jsx
@@ -1,8 +1,10 @@
-
-import React from 'react'
+import React from "react";
 
 export default function ProjectNavigation() {
   return (
-    <div>ProjectNavigation</div>
-  )
+    <>
+      <h1>ProjectNavigation</h1>
+
+    </>
+  );
 }
