415bed8db115d7f46764a8833e39c27708d6f912
Tahir <quaidcodes@gmail.com>
2024-04-22 04:17:56 -0400

    work on project component

--- src/components/ProjectsDir/ProjectNavigation/ProjectNavigation.jsx
+++ src/components/ProjectsDir/ProjectNavigation/ProjectNavigation.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import "./ProjectNavigation.css";
 
 export default function ProjectNavigation() {
   return (
-    <>
-      <h1>ProjectNavigation</h1>
+    <div className="project-navigation">
+      <Link to="/projects/banking-app">Banking App</Link>
+      <Link to="/projects/pos-app">POS System</Link>
 
-    </>
+      <h1>hello</h1>
+      <Outlet />
+    </div>
   );
 }
--- src/components/ProjectsDir/Projects.jsx
+++ src/components/ProjectsDir/Projects.jsx
@@ -1,8 +1,10 @@
 
+import "./Projects.css";
+
 function Projects() {
-    return <>
-    
-    </>
+    return <section className="projects">
+        <h1>Projects Main</h1>
+    </section>
 }
 
 export default Projects
